1ea9bb1539ad50be1c025158a24f8928a4c94834
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2022-12-08 00:51:20 +0330

    Default switch checkbox input

--- src/Calculator/Components/Bar.jsx
+++ src/Calculator/Components/Bar.jsx
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
+import bootstrap from "react-bootstrap";
 
 class Bar extends Component {
   bar = () => {
     console.log("show Bar");
   };
-  
-  handleOnClick=() => {
- console.log("dark & light mode");
 
- };
+  handleOnClick = () => {
+    console.log("dark & light mode");
+  };
   render() {
     return (
-    <div className="bar">
-      {/* <div className="icon"> */}
-     <button className="icon" onClick={this.handleOnClick}>Them mode</button>
-        <h2> ماشین حساب مهندسی</h2>
-     </div>
-    //  </div>
-
-    )
+      <div className="bar">
+        <div className="theme" class="form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+          />
+          <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+        </div>
+        <h4> ماشین حساب مهندسی</h4>
+      </div>
+      //  </div>
+    );
   }
-};
-
-
+}
 
 export default Bar;
