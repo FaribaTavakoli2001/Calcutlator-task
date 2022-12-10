3ca3c8e329f395ba8773bca18c1bb4652a47073b
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2022-12-11 00:37:30 +0330

    edit css style

--- src/Calculator/Components/Bar.jsx
+++ src/Calculator/Components/Bar.jsx
@@ -12,15 +12,32 @@ class Bar extends Component {
   render() {
     return (
       <div className="bar">
-        <div className="theme" class="form-check form-switch">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            role="switch"
-            id="flexSwitchCheckDefault"
-          />
-          <label class="form-check-label" for="flexSwitchCheckDefault"></label>
-        </div>
+        <form className="Switch">
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              class="form-check-label"
+              for="flexSwitchCheckDefault"
+            ></label>
+          </div>
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckChecked"
+            />
+            <label
+              class="form-check-label"
+              for="flexSwitchCheckChecked"
+            ></label>
+          </div>
+        </form>
         <h4> ماشین حساب مهندسی</h4>
       </div>
       //  </div>
