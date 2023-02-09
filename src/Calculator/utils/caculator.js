20fded863e186298e397b134909e0f5528d4150b
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2023-02-09 16:31:16 +0330

    converting infix to postfix with stack

--- src/Calculator/utils/caculator.js
+++ src/Calculator/utils/caculator.js
@@ -1,28 +1,82 @@
-const simpleCalculate = (formula) => {
-  //TODO: not yet implemented
-  // FIXME:
-};
+// const simpleCalculate = (formula) => {
+//   //TODO: not yet implemented
+//   // FIXME:
+// };
 
-const findInternalParentheses = (formula) => {
-  //TODO: not yet implemented
-};
+// const findInternalParentheses = (formula) => {
+//   //TODO: not yet implemented
+// };
 
-export function calculate(formula = "") {
-  const [firstIndex, lastIndex] = findInternalParentheses(formula);
+// export function calculate(formula = "") {
+//   const [firstIndex, lastIndex] = findInternalParentheses(formula);
 
-  // TODO: do this while any Parentheses exists
+//   // TODO: do this while any Parentheses exists
 
-  const nonParenthesesFormula = formula.substring(firstIndex + 1, lastIndex);
-  const result = simpleCalculate(nonParenthesesFormula);
+//   const nonParenthesesFormula = formula.substring(firstIndex + 1, lastIndex);
+//   const result = simpleCalculate(nonParenthesesFormula);
 
-  const part1 = formula.substring(0, firstIndex - 1);
-  const part2 = formula.substring(lastIndex + 1);
-  const newFormula = part1 + result + part2;
+//   const part1 = formula.substring(0, firstIndex - 1);
+//   const part2 = formula.substring(lastIndex + 1);
+//   const newFormula = part1 + result + part2;
 
-  return formula;
+//   return formula;
+// }
+
+// export function main() {
+//   console.log(simpleCalculate("2+3"));
+// }
+
+
+// 
+const infixTopostfix = (infixexpr) => {
+  let prec = {};
+
+  prec["*"] = 3;
+  prec["/"] = 3;
+  prec["+"] = 2;
+  prec["-"] = 2;
+  prec["("] = 1;
+
+  let opStack = [];
+  let postFixList = [];
+
+  let tokenList = infixexpr.split();
+  let NumberList = "0123456789".split("");
+  let topToken;
+
+  for (let i = 0; i < tokenList.length; i++) {
+    NumberList.forEach((num) => {
+      if (tokenList[i] === num) postFixList.push(tokenList[i]);
+      else if (tokenList[i] === "(") opStack.push(tokenList[i]);
+      else if (tokenList[i] === ")") {
+        topToken = opStack.pop();
+        while (topToken != "(") {
+          postFixList.push(topToken);
+          topToken = opStack.pop();
+        }
+      } else {
+        while (
+          opStack.length != 0 &&
+          prec[opStack[opStack.length - 1]] >= prec[tokenList[i]]
+        ) {
+          postFixList.push(opStack.pop());
+        }
+
+        opStack.push(tokenList[i]);
+      }
+    });
+  }
+
+  while (opStack.length != 0) {
+    postFixList.push(opStack.pop());
+  }
+
+  return postFixList.join("");
+
 }
 
+
 export function main() {
-  console.log(simpleCalculate("2+3"));
+  console.log(infixTopostfix("3+2"))
 }
 
